4220c5a73d739b89d3a4bb9c4ac6128ed1a76e7e
BrissAcevedo <22300036@uttt.edu.mx>
2025-10-13 17:53:46 -0600

    BC: Sprint 3 Brisa Crud cartelera

---
+++ Script Crud Cartelera.sql
@@ -0,0 +1,17 @@
+﻿USE CleanArchDb;
+GO
+
+-- 🔹 Crear la tabla Peliculas
+CREATE TABLE [dbo].[Peliculas] (
+    [IdPelicula] INT IDENTITY(1,1) PRIMARY KEY,
+    [Titulo] NVARCHAR(150) NOT NULL,
+    [Descripcion] NVARCHAR(MAX) NULL,
+    [Duracion] INT NULL,                -- duración en minutos
+    [Clasificacion] NVARCHAR(50) NULL,  -- ejemplo: PG-13, R, A, B, C
+    [Genero] NVARCHAR(100) NULL,        -- ejemplo: Acción, Drama, Comedia
+    [Estado] NVARCHAR(50) NULL,         -- ejemplo: Activa, Inactiva
+    [FechaEstreno] DATE NULL,
+    [ImagenUrl] NVARCHAR(250) NULL      -- ruta o URL de la imagen
+);
+GO
+
